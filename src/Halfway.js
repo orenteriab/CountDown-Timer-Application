ad1e436f13884875e8798911a958c9654b20b5c3
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-05-02 23:40:06 -0600

    grouping to follow conventions

--- src/Halfway.js
+++ src/Halfway.js
@@ -14,7 +14,7 @@ import { convertFormattedTimeToRawTime } from './util';
 const Halfway = ({ time, proposedTime, started }) => {
   const timeProgress = time / convertFormattedTimeToRawTime(proposedTime);
   const label = started && (timeProgress <= 0.5) ? 'More than halfway there!' : null;
-  const labelRendering = label !== null ? <span>{label}</span> : null;
+  const labelRendering = label !== null ? (<span>{label}</span>) : null;
 
   return (
     <div className="halfway-container">
