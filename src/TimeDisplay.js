57f6c3ef14029d43e703963efc3f83131d15c833
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-05-02 22:43:40 -0600

    using function from util file

--- src/TimeDisplay.js
+++ src/TimeDisplay.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-/**
- * If a number is passed then, format it to show in
- * the display.
- * @param {number} unit
- */
-const makeTimeUnitDisplayable = (unit) => {
-  return unit > 9 ? String(unit) : `0${unit}`;
-};
-
-/**
- * It converts the number of seconds to a formatted
- * `mm:ss` string.
- * @param {number} seconds
- */
-const convertRawTime = (rawSeconds) => {
-  if (isNaN(rawSeconds)) return rawSeconds;
-
-  const minutes = Math.floor(rawSeconds / 60);
-  const seconds = rawSeconds - (minutes * 60);
-  const displayableMinutes = makeTimeUnitDisplayable(minutes);
-  const displayableSeconds = makeTimeUnitDisplayable(seconds);
-  return `${displayableMinutes}:${displayableSeconds}`;
-};
+import { convertRawTime } from './util';
 
 const TimeDisplay = ({ run, started, time, onCountdownClick }) => {
   const countdownIcon = !run ? 'play-circle' : 'pause-circle';
