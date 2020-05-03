6d3c233549bbbc05f005a8a9922b684f4dbbb5ae
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-05-02 23:24:21 -0600

    Typehints for halfway component

--- src/Halfway.js
+++ src/Halfway.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { convertFormattedTimeToRawTime } from './util';
 
+/**
+ * This component is responsible for the notification of
+ * when the time is past halfway.
+ * @typedef {{
+ *  time: number,
+ *  proposedTime: string,
+ *  started: boolean
+ * }} Props
+ * @param {Props} props
+ */
 const Halfway = ({ time, proposedTime, started }) => {
   const timeProgress = time / convertFormattedTimeToRawTime(proposedTime);
   const label = started && (timeProgress <= 0.5) ? 'More than halfway there!' : null;
