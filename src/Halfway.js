04b4027275032fe41c9028c3a36ac59dadb13939
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-05-02 22:44:27 -0600

    using util function

--- src/Halfway.js
+++ src/Halfway.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { convertFormattedTimeToRawTime } from './util';
 
 const Halfway = ({ time, proposedTime, started }) => {
-  const timeProgress = time / proposedTime;
+  const timeProgress = time / convertFormattedTimeToRawTime(proposedTime);
   const label = started && (timeProgress <= 0.5) ? 'More than halfway there!' : null;
   const labelRendering = label !== null ? <span>{label}</span> : null;
 
