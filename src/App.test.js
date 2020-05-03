5b20eccdbd85fc7674aab6cb77844f2aca4aaee4
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-05-03 11:26:43 -0600

    Smoke tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,42 @@
-import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
+import Halfway from './Halfway';
+import TimeDisplay from './TimeDisplay';
+import TimeInput from './TimeInput';
+import TimeSpeed from './TimeSpeed';
+import React from 'react';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders app with state values', () => {
+  const rootWrapper = shallow(<App />);
+  const appInstance = rootWrapper.instance();
+  const {
+    time,
+    proposedTime,
+    started,
+    run,
+    canStart,
+    speed
+  } = rootWrapper.state()
+  const timeInput = (<TimeInput
+    onCountdownChange={appInstance.onCountdownChange}
+    started={started}
+    proposedTime={proposedTime}
+    onStartClick={appInstance.onStartClick} 
+    canStart={canStart} />);
+  const halfway = (<Halfway
+    time={time}
+    proposedTime={proposedTime}
+    started={started} />);
+  const timeDisplay = (<TimeDisplay 
+    run={run}
+    started={started}
+    time={time}
+    onCountdownClick={appInstance.onCountdownClick} />);
+  const timeSpeed = (<TimeSpeed
+    started={started}
+    speed={speed}
+    onSpeedChange={appInstance.onSpeedChange}
+    run={run} />);
+  const children = [timeInput, halfway, timeDisplay, timeSpeed];
+  expect(rootWrapper.contains(...children)).toEqual(true);
 });
