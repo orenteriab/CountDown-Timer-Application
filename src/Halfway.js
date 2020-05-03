fac7e50c1d126d4bf1a03a1cc42623c22fa5b096
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-05-03 17:27:18 -0600

    Moving time's up and refactoring tests

--- src/Halfway.js
+++ src/Halfway.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { convertFormattedTimeToRawTime } from './util';
 
 /**
  * This component is responsible for the notification of
@@ -12,8 +11,10 @@ import { convertFormattedTimeToRawTime } from './util';
  * @param {Props} props
  */
 const Halfway = ({ time, proposedTime, started }) => {
-  const timeProgress = time / convertFormattedTimeToRawTime(proposedTime);
-  const label = started && (timeProgress <= 0.5) ? 'More than halfway there!' : null;
+  const timeProgress = time / (parseInt(proposedTime) * 60);
+  const isProgressHalfOrLess = timeProgress <= 0.5;
+  const timesUpOrNull = proposedTime === '0' ? 'Time\'s up!' : null;
+  const label = started && isProgressHalfOrLess ? 'More than halfway there!' : timesUpOrNull;
   const labelRendering = label !== null ? (<span>{label}</span>) : null;
 
   return (
--- src/Halfway.test.js
+++ src/Halfway.test.js
@@ -2,23 +2,57 @@ import { shallow } from 'enzyme';
 import Halfway from './Halfway';
 import React from 'react';
 
-test('Must show label', () =>{
-  const wrapper = shallow(<Halfway proposedTime="00:50" time={25} started={true} />);
+/**
+ * Is the template for the test
+ * @param {boolean} labelExpected
+ */
+const testBuilder = (wrapper, labelExpected) => {
   const container = wrapper.find('div.halfway-container');
   expect(container.exists()).toBe(true);
 
   const label = container.find('span');
-  expect(label.exists()).toBe(true);
+  expect(label.exists()).toBe(labelExpected);
 
-  const { children } = label.props();
-  expect(children).toEqual('More than halfway there!');
+  return label;
+};
+
+/**
+ * Basic test structure builder.
+ * @param {string} proposedTime
+ * @param {number} time
+ * @param {boolean} started
+ * @param {boolean} labelExpected
+ * @param {string} labelValue
+ */
+const structureBuilder = (proposedTime, time, started, labelExpected, labelValue) => {
+  const wrapper = shallow(<Halfway
+    proposedTime={proposedTime}
+    time={time}
+    started={started} />);
+  const label = testBuilder(wrapper, labelExpected);
+
+  if (labelExpected) {
+    const { children } = label.props();
+    expect(children).toEqual(labelValue);
+  }
+};
+
+describe('Must show', () => {
+  test('Halfway label', () => {
+    structureBuilder("1", 30, true, true, 'More than halfway there!');
+  });
+
+  test('Time\'s up', () => {
+    structureBuilder("0", 0, false, true, 'Time\'s up!');
+  });
 });
 
-test('Must not show label', () =>{
-  const wrapper = shallow(<Halfway proposedTime="00:50" time={26} started={true} />);
-  const container = wrapper.find('div.halfway-container');
-  expect(container.exists()).toBe(true);
+describe('Must not show', () => {
+  test('More than 50%', () => {
+    structureBuilder("1", 31, true, false);
+  });
 
-  const label = container.find('span');
-  expect(label.exists()).toBe(false);
+  test('Initial load', () => {
+    structureBuilder("", 0, false, false);
+  });
 });
