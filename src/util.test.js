81122ea5aca459ea8b9b476b47c1b0380efc28c3
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2020-05-03 21:35:35 -0600

    Removing unnecessary test

--- src/util.test.js
+++ src/util.test.js
@@ -1,19 +1,12 @@
 import { convertRawTime } from './util';
 
-describe('Convert raw Time', () => {
-  test('Testing with number', () => {
-    const formatted01 = convertRawTime(100);
-    expect(formatted01).toEqual('01:40');
+test('Convert raw Time', () => {
+  const formatted01 = convertRawTime(100);
+  expect(formatted01).toEqual('01:40');
 
-    const formatted02 = convertRawTime(60);
-    expect(formatted02).toEqual('01:00');
+  const formatted02 = convertRawTime(60);
+  expect(formatted02).toEqual('01:00');
 
-    const formatted03 = convertRawTime(170);
-    expect(formatted03).toEqual('02:50');
-  });
-
-  test('Time\'s up', () => {
-    const formattedTimesUp = convertRawTime('Time\'s up');
-    expect(formattedTimesUp).toEqual('Time\'s up');
-  });
+  const formatted03 = convertRawTime(170);
+  expect(formatted03).toEqual('02:50');
 });
